8e08c99b238ab1f45161be6b004212a568e279cc
xLyaso <61823523+xLyaso@users.noreply.github.com>
2020-12-01 15:11:48 +0100

    update

--- lieferant1/src/restapi/RestConfig.java
+++ lieferant1/src/restapi/RestConfig.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 public class RestConfig
 {
@@ -19,11 +23,23 @@ public class RestConfig
         return handlebarType+handlebarMaterial+handlebarGearshift+handleMaterial;
     }
 
-
     public String calculateDatePrice()
     {
+        Random r = new Random();
+        int low = 120;
+        int high = 400;
+        int result = r.nextInt(high-low) + low;
 
-        
-        return "";
+        System.out.println(result);
+
+        LocalDate startDate = LocalDate.now();
+        long start = startDate.toEpochDay();
+        LocalDate endDate = LocalDate.of(2021, 12, 31);
+        long end = endDate.toEpochDay();
+
+        long randomEpochDay = ThreadLocalRandom.current().longs(start, end).findAny().getAsLong();
+        //System.out.println(LocalDate.ofEpochDay(randomEpochDay));
+
+        return ""+result + LocalDate.ofEpochDay(randomEpochDay);
     }
 }
